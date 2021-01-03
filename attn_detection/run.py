b9f29f9d00fca6ab344591c54d046d5ce597e7ac
ecal <ecallanan04@gmail.com>
2021-01-03 17:37:21 -0500

    add seperate script for running all models

--- attn_detection/run.py
+++ attn_detection/run.py
@@ -1,5 +1,13 @@
-import * from face_detector
+from detector import *
 
-main():
+
+def main():
     detector = FaceDetector()
+    try:
+        box, prob, ld = detector.detect()
+        print(f"face detected with {(prob[0] * 100):.2f}% certainty")
+    except:
+        print("No face detected")
+
+
 main()

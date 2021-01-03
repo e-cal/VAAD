3029fd770c561fc69f48b388d32861850e89449e
ecal <ecallanan04@gmail.com>
2021-01-03 18:15:41 -0500

    cleanup run.py

--- attn_detection/run.py
+++ attn_detection/run.py
@@ -5,11 +5,10 @@ def main():
     detector = FaceDetector()
     prob = detector.detect()
     if prob == None:
-        prob = 0
         print("No face detected")
-    else:
-        prob = prob[0]
-        print(f"face detected with {(prob * 100):.2f}% certainty")
+        return
+
+    print(f"face detected with {(prob * 100):.2f}% certainty")
 
 
 main()

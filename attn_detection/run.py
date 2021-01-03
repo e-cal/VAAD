832fbfdf3af2626ec7cb53ca698bc63eda6e4ac2
ecal <ecallanan04@gmail.com>
2021-01-03 17:43:33 -0500

    Revert "add seperate script for running all models"
    This reverts commit 9a5167b0710dbe62b0b96b8ad26ce0f2ca09191f.

--- attn_detection/run.py
+++ attn_detection/run.py
@@ -3,9 +3,11 @@ from detector import *
 
 def main():
     detector = FaceDetector()
-    box, prob, ld = detector.detect()
-    print(f"face detected with {(prob[0] * 100):.2f}% certainty")
-    print("No face detected")
+    try:
+        box, prob, ld = detector.detect()
+        print(f"face detected with {(prob[0] * 100):.2f}% certainty")
+    except:
+        print("No face detected")
 
 
 main()

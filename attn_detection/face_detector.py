988677f5e95859ef4bc6f0dccf9663300b95cbf0
ecal <ecallanan04@gmail.com>
2021-01-03 16:48:36 -0500

    update face_detector.py

---
+++ attn_detection/face_detector.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cv2
+import torch
+from facenet_pytorch import MTCNN
+
+
+class FaceDetector(object):
+    def __init__(self):
+        self.mtcnn = MTCNN()
+
+    def detect(self):
+        video = cv2.VideoCapture(0)
+        ret, frame = video.read()
+        box, prob, ld = self.mtcnn.detect(frame, landmarks=True)
+        if not type(box) == type(None):
+            video.release()
+            return box, prob, ld
+        video.release()
+
+    def overlay(self, frame, box, prob):
+        for box, prob in zip(box, prob):
+            cv2.rectangle(frame,
+                          (box[0], box[1]),
+                          (box[2], box[3]),
+                          (0, 0, 255),
+                          thickness=2)
+
+            cv2.putText(frame,
+                        str(prob),
+                        (box[2], box[3]),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 0, 255), 2,
+                        cv2.LINE_AA)
+
+    def run(self):
+        video = cv2.VideoCapture(0)
+        while True:
+            ret, frame = video.read()
+            box, prob, ld = self.mtcnn.detect(frame, landmarks=True)
+            if not type(box) == type(None):
+                print(f"box: {box}\nprobs: {prob}\n")
+                self.overlay(frame, box, prob)
+            else:
+                print("No face detected")
+            cv2.imshow('Face Detector', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        video.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    detector = FaceDetector()
+    try:
+        box, prob, ld = detector.detect()
+        print(f"face detected with {(prob[0] * 100):.2f}% certainty")
+    except:
+        print("No face detected")
+    # detector.run()

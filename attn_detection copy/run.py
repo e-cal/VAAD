2e6b8a87891c25fbfeed6fca097e4eb59b3d5334
Kavin Zhu <kavinzhu@Kavins-MacBook-Air.local>
2021-01-07 19:20:55 -0500

    added a streamlit demo

---
+++ attn_detection copy/run.py
@@ -0,0 +1,15 @@
+from detector import FaceDetector
+
+
+def main():
+    detector = FaceDetector()
+    detector.run()
+#    prob = detector.detect()
+#    if prob == None:
+#        print("No face detected")
+#        return
+#
+#    print(f"face detected with {(prob * 100):.2f}% certainty")
+
+
+main()

1b969e87b3d0cc8bce47c8daa65f42485823719e
ecal <ecallanan04@gmail.com>
2021-01-09 20:31:21 -0500

    Finish training script and train a model. Add script to test the model

--- attn_detection/train_classifier.py
+++ attn_detection/train_classifier.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, transforms, models
 
+DATA_DIR = "data/"
+
 
 def split_data(data_path, valid_size=0.2):
     train_transforms = transforms.Compose(
@@ -29,6 +31,75 @@ def split_data(data_path, valid_size=0.2):
     return trainloader, testloader
 
 
-DATA_DIR = "/data"
-trainloader, testloader = split_data(DATA_DIR)
-print(trainloader.dataset.classes)
+def main():
+    # Define dataset loaders
+    trainloader, testloader = split_data(DATA_DIR)
+    print(f"Classes: {trainloader.dataset.classes}")
+
+    # Use GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load pretrained model (ResNet50)
+    model = models.resnet50(pretrained=True)
+    print("resnet50 model loaded")
+
+    # Freeze pretrained layers and define final layer (to be trained)
+    for param in model.parameters():
+        param.requires_grad = False
+    model.fc = nn.Sequential(
+        nn.Linear(2048, 512),
+        nn.ReLU(),
+        nn.Dropout(0.2),
+        nn.Linear(512, 10),
+        nn.LogSoftmax(dim=1)
+    )
+
+    # Create loss function and optimizer with a learning rate
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+    model.to(device)
+
+    # Train
+    epochs = 10
+    steps = 0
+    running_loss = 0
+    print_every = 10
+    train_losses, test_losses = [], []
+    for epoch in range(epochs):
+        for inputs, labels in trainloader:
+            steps += 1
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+                        test_loss += batch_loss.item()
+
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                train_losses.append(running_loss/len(trainloader))
+                test_losses.append(test_loss/len(testloader))
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                      f"Train loss: {running_loss/print_every:.3f}.. "
+                      f"Test loss: {test_loss/len(testloader):.3f}.. "
+                      f"Test accuracy: {accuracy/len(testloader):.3f}")
+                running_loss = 0
+                model.train()
+    torch.save(model, 'attention_model.pth')
+
+
+main()

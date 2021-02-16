a1d5143ca6d15bb8be1eb0e0f346442d79d05488
ecal <ecallanan04@gmail.com>
2021-02-16 15:18:06 -0500

    train classifier on mit data + me

---
+++ model/run.py
@@ -0,0 +1,20 @@
+from detector import FaceDetector
+
+
+def main():
+    detector = FaceDetector()
+#    prob = detector.detect()
+#    if prob == None:
+#        print("No face detected")
+#        return
+#
+#    print(f"face detected with {(prob * 100):.2f}% certainty")
+    prob = detector.detect()
+    if not prob:
+        print("No face detected")
+        return
+
+    print(f"face detected with {(prob * 100):.2f}% certainty")
+
+
+main()
